215ecec730306cf0f8a43c765221c729e1796784
Tulasi Shukla <tulsishuklag@gmail.com>
2025-06-26 18:20:23 +0530

    Update page.js

--- app/[shorturl]/page.js
+++ app/[shorturl]/page.js
@@ -1,22 +1,27 @@
-import { redirect } from "next/navigation"
-import clientPromise from "@/lib/mongodb"
 
+
+import { notFound, redirect  } from "next/navigation"
+import clientPromise from "@/lib/mongodb"
 
 export default async function Page({ params }) {
     const shorturl = (await params).shorturl
-
+   
     const client = await clientPromise;
     const db = client.db("bitlinks")
     const collection = db.collection("url")
+    const doc = await collection.findOne({shorturl})
 
-    const doc = await collection.findOne({shorturl: shorturl})
+   
     console.log(doc)
-    if(doc){
-         redirect(doc.url)
-    }
-    else{
-        redirect(`${process.env.NEXT_PUBLIC_HOST}`)
-    }
+   
 
-    return <div>My Post: {url}</div>
-  }
+    if(!doc){
+     
+        console.log("not found ", shorturl);
+        notFound();
+
+    }
+   
+console.log("redirect to", doc.url)
+redirect(doc.url)
+    }
